034eb164ac748a94a1c86591faf7bc60e5d618cf
Arbaoui Walid <walid.arbaoui@emsi-edu.ma>
2024-12-26 20:50:48 +0100

    banner

--- lib/Widget/banner.dart
+++ lib/Widget/banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/Utils/constants.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:recipe_app/Views/meal_plan_screen.dart';
 import 'package:recipe_app/services/notifs_service.dart';
 
 class BannerToExplore extends StatefulWidget {
@@ -24,7 +25,7 @@ class _BannerToExploreState extends State<BannerToExplore> {
       List<Map<String, dynamic>>? mostRecent =
           await notifsService.fetchMostRecentDateWithRecipe();
       recent = mostRecent.first;
-      print(recent);
+      print('·············: $recent');
     } catch (e) {
       print('Error: $e');
     }
@@ -39,158 +40,163 @@ class _BannerToExploreState extends State<BannerToExplore> {
         borderRadius: BorderRadius.circular(15),
         color: kBannerColor,
         image: DecorationImage(
-            image: NetworkImage(
-              recent?['image'], // image from firestore
-            ),
+            image: recent?.isEmpty == false
+                ? NetworkImage(
+                    recent?['image'], // image from firestore
+                  )
+                : const AssetImage('assets/images/welcome-no-filter.png'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Theme.of(context).colorScheme.primary, BlendMode.overlay),
             opacity: 0.2,
             alignment: Alignment.topRight),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Your Next Meal.',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 25),
-            SizedBox(
-              width: double.infinity,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          recent?['name'],
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w500),
+      child: recent?.isEmpty == false
+          ? Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Your Next Meal.',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 25),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                recent?['name'],
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Iconsax.flash_1,
+                                    size: 20,
+                                    color: Colors.white,
+                                  ),
+                                  Text(
+                                    "${recent?['cal']} Cal",
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const Text(
+                                    " · ",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w900,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Iconsax.clock,
+                                    size: 20,
+                                    color: Colors.white,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    "${recent?['time']} Min",
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                recent?['dateRecipe'],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Text(
+                                recent?['timeRecipe'],
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                    height: 0.7),
+                              ),
+                            ],
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+            )
+          : Stack(
+              children: [
+                Positioned(
+                  top: 32,
+                  left: 20,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Cook the best\nrecipes at home",
+                        style: TextStyle(
+                          height: 1.1,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                        Row(
-                          children: [
-                            const Icon(
-                              Iconsax.flash_1,
-                              size: 20,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              "${recent?['cal']} Cal",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                            const Text(
-                              " · ",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                color: Colors.white,
-                              ),
-                            ),
-                            const Icon(
-                              Iconsax.clock,
-                              size: 20,
-                              color: Colors.white,
-                            ),
-                            const SizedBox(width: 5),
-                            Text(
-                              "${recent?['time']} Min",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 33,
+                          ),
+                          backgroundColor: Colors.white,
+                          elevation: 0,
                         ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          recent?['dateRecipe'],
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
+                        onPressed: () {
+                          
+                        },
+                        child: const Text(
+                          "Make plans",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
-                        Text(
-                          recent?['timeRecipe'],
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                              height: 0.7),
-                        ),
-                      ],
-                    ),
-                  ]),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  bottom: 0,
+                  right: -20,
+                  child: Image.network(
+                    "https://pngimg.com/d/chef_PNG190.png",
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      // Stack(
-      //   children: [
-      //     Positioned(
-      //       top: 32,
-      //       left: 20,
-      //       child: Column(
-      //         crossAxisAlignment: CrossAxisAlignment.start,
-      //         children: [
-      //           const Text(
-      //             "Cook the best\nrecipes at home",
-      //             style: TextStyle(
-      //               height: 1.1,
-      //               fontSize: 22,
-      //               fontWeight: FontWeight.bold,
-      //               color: Colors.white,
-      //             ),
-      //           ),
-      //           const SizedBox(height: 10),
-      //           ElevatedButton(
-      //             style: ElevatedButton.styleFrom(
-      //               padding: const EdgeInsets.symmetric(
-      //                 horizontal: 33,
-      //               ),
-      //               backgroundColor: Colors.white,
-      //               elevation: 0,
-      //             ),
-      //             onPressed: () {},
-      //             child: const Text(
-      //               "Explore",
-      //               style: TextStyle(
-      //                 fontSize: 15,
-      //                 fontWeight: FontWeight.bold,
-      //                 color: Colors.black,
-      //               ),
-      //             ),
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //     Positioned(
-      //       top: 0,
-      //       bottom: 0,
-      //       right: -20,
-      //       child: Image.network(
-      //         "https://pngimg.com/d/chef_PNG190.png",
-      //       ),
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
